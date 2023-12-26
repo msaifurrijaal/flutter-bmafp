e0a3ed138e704cc0c33aedadf54fc5b790c4153d
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-12-26 08:17:23 +0700

    refactor: atribut card

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_apps/models/football_club.dart';
 import 'package:football_apps/theme.dart';
+import 'package:football_apps/widgets/atribut_card.dart';
 import 'package:football_apps/widgets/favorite_button.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -148,35 +149,6 @@ class DetailPage extends StatelessWidget {
             ),
           )
         ],
-      ),
-    );
-  }
-}
-
-class AtributCard extends StatelessWidget {
-  final String text;
-  final Icon icon;
-
-  const AtributCard({super.key, required this.text, required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      style: ButtonStyle(
-          padding: const MaterialStatePropertyAll(
-            EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-          ),
-          shape: MaterialStatePropertyAll(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))),
-          elevation: const MaterialStatePropertyAll(0),
-          foregroundColor: const MaterialStatePropertyAll(blueColor),
-          backgroundColor: const MaterialStatePropertyAll(Color(0xffEFF8FF))),
-      onPressed: () {},
-      icon: icon,
-      label: Text(
-        text,
-        style:
-            Theme.of(context).textTheme.bodySmall?.copyWith(color: blueColor),
       ),
     );
   }
---
+++ lib/widgets/atribut_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:football_apps/theme.dart';
+
+class AtributCard extends StatelessWidget {
+  final String text;
+  final Icon icon;
+
+  const AtributCard({super.key, required this.text, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      style: ButtonStyle(
+          padding: const MaterialStatePropertyAll(
+            EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          ),
+          shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))),
+          elevation: const MaterialStatePropertyAll(0),
+          foregroundColor: const MaterialStatePropertyAll(blueColor),
+          backgroundColor: const MaterialStatePropertyAll(Color(0xffEFF8FF))),
+      onPressed: () {},
+      icon: icon,
+      label: Text(
+        text,
+        style:
+            Theme.of(context).textTheme.bodySmall?.copyWith(color: blueColor),
+      ),
+    );
+  }
+}
